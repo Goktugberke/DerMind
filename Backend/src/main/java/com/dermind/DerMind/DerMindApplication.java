746ff30f5d93158ff6b4b52885f00c1659c2adbd
Göktuğ Berke Güngören <gungorengoktugberke@gmail.com>
2025-10-17 17:23:51 +0300

    Initializes backend project structure
    Sets up the basic project structure for the backend using Spring Boot, including necessary files like .gitignore, .gitattributes, pom.xml, application.properties, and wrapper files.
    This commit lays the foundation for future backend development.

---
+++ Backend/src/main/java/com/dermind/DerMind/DerMindApplication.java
@@ -0,0 +1,13 @@
+package com.dermind.DerMind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DerMindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DerMindApplication.class, args);
+	}
+
+}
