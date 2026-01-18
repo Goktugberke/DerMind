cab6b3ad923214999f7bf4a8867983045e12ef53
Derya Salihoglu <deryaslhoglu.54@gmail.com>
2026-01-18 17:45:17 +0300

    theme and style setup

---
+++ mobile/src/constants/theme.ts
@@ -0,0 +1,41 @@
+export const theme = {
+  colors: {
+    // Main colors
+    primary: '#FFC1C1',
+    secondary: '#8A226F',
+
+    // Neutral colors
+    background: '#F8F8F8',
+    deepbackground: '#E0E0E0',
+    white: '#FFFFFF',
+    text: '#2D3436',
+    gray: '#636E72',
+    lightGray: '#DFE6E9',
+
+    // Status colors
+    error: '#D63031',
+    warning: '#557ae1ff',
+    success: '#09b800ff',
+  },
+  spacing: {
+    xs: 4,
+    s: 8,
+    m: 16,
+    l: 24,
+    xl: 32,
+  },
+  fontSize: {
+    small: 12,
+    medium: 16,
+    large: 20,
+    title: 28,
+  },
+  borderRadius: {
+    small: 4,
+    medium: 8,
+    large: 12,
+    round: 25,
+  }
+};
+
+export type Theme = typeof theme;
